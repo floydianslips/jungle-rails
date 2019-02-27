265fa883c7f617a53099aed5150a1822bc825706
joshua <josh@joshuadennis.ca>
2019-02-27 02:08:47 +0000

    product details test is functioning

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
+RSpec.feature "ProductDetails", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -10,20 +9,21 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
-        image: open_asset('apparel1.jpg'), 
+        image: open_asset('apparel1.jpg'),
         quantity: 10,
         price: 64.99
       )
-      
     end
   end
 
-  scenario "They see all products" do
+  scenario "Clicking on product directs the user to that product's page" do
     # ACT
     visit root_path
+    first('a.product-link').click
+    # DEBUG / VERIFY
+    expect(page).to have_css '.products-show'
+ 
     save_screenshot
-
-    # VERIFY
-    expect(page).to have_css 'article.product', count: 10
-  end
+ end
 end
+
